6d2f4cd33fcea69fb382997f14d1bfe9e649c499
Reliance BE <136088232+RelianceBE-Code-Base@users.noreply.github.com>
2023-08-30 10:36:11 +0100

    Update config.tsx

--- Source/CompanyCommunicator/ClientApp/src/components/config.tsx
+++ Source/CompanyCommunicator/ClientApp/src/components/config.tsx
@@ -49,7 +49,7 @@ class Configuration extends React.Component<ConfigProps, IConfigState> {
             microsoftTeams.settings.setSettings({
                 entityId: "Company_Communicator_App",
                 contentUrl: this.state.url,
-                suggestedDisplayName: "Company Communicator",
+                suggestedDisplayName: "HR Engage",
             });
             saveEvent.notifySuccess();
         });
