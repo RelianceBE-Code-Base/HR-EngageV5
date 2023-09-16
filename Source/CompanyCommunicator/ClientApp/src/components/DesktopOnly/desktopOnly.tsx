b7279a0bf6e441eb41aad9d028135b89955676f1
Reliance BE <136088232+RelianceBE-Code-Base@users.noreply.github.com>
2023-09-16 16:18:48 +0100

    Add files via upload

---
+++ Source/CompanyCommunicator/ClientApp/src/components/DesktopOnly/desktopOnly.tsx
@@ -0,0 +1,15 @@
+﻿import React from 'react'
+import "./desktopOnly.scss";
+import imgPlaceholder from "./placeholder-desktop.png";
+
+
+function DesktopOnly() {
+    return (
+        <div className='desktop-view-placeholder'>
+            <img src={imgPlaceholder} alt="" />
+            <h1 className='placeholder-text'>This view is only available on desktop</h1>
+        </div>
+    )
+}
+
+export default DesktopOnly
